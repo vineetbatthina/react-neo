734d73eb4a94bde5f588dddf8470e97d580c64e1
vinnubatthina <vinnu.batthina@gmail.com>
2021-01-29 09:26:31 +0530

    videos complete

--- searchvideos/src/components/SearchBar.js
+++ searchvideos/src/components/SearchBar.js
@@ -22,7 +22,7 @@ class SearchBar extends React.Component{
             <div className="search-bar ui segment">
                 <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
-                        <label>Image Search</label>
+                        <label>Video Search</label>
                         <input type="text" value={this.state.term} onChange={this.onTermChanged}/>
                     </div>
                 </form>
